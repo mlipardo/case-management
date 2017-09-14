56889ea7fc14fb853a7564c2d0956174d89937f9
Danny Eck <daniel.eck@accenture.com>
2017-09-14 02:00:13 -0700

    Add basic todo app (redux)
    Adds the `todos` app from the `redux` readme [Usage with
    React](http://redux.js.org/docs/basics/UsageWithReact.html) section.
    Pretty much a straight import. Just want to get familiar with Rails
    interface(s) and use for demo (e.g.; time travel debugging)

--- app/javascript/emergency-placement/index.js
+++ app/javascript/emergency-placement/index.js
@@ -1,1 +1,18 @@
-export { default as App } from './app';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
+
+let store = createStore(
+  todoApp,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.body.appendChild(document.createElement('div'))
+)
--- app/javascript/packs/emergency-placement.js
+++ app/javascript/packs/emergency-placement.js
@@ -1,10 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {App} from '../emergency-placement';
-
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <App name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
-});
+import '../emergency-placement';
