4a7cbe0d2b452169abb07a2279d46f96b49d089e
Ed Gibbs <edward_gibbs@yahoo.com>
2017-11-01 04:20:55 -0700

    Fix test for now

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature "index page" do
   scenario "displays default rails" do
     visit "/"
-    expect(page).to have_content('Hello Not')
+    expect(page).to have_content('Hello')
   end
 end
