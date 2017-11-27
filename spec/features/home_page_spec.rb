3971504b79b4ed90ffe27917476f5b4f70b5f72e
Melbourne Lipardo <melbourne.lipardo@osi.ca.gov>
2017-11-27 13:30:17 -0800

    Rspec for landing page

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 feature "index page" do
-  scenario "displays default rails" do
+  scenario "displays cm landing page" do
     visit "/"
-    expect(page).to have_content('Hello')
+    expect(page).to have_content('Dashboard')
   end
 end
