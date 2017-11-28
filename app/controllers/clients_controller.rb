a45efd2ad72e34dae124af5082d128998e56a97c
Ramu Kammagani <cambriadeveloper@Cambrias-MacBook-Pro.local>
2017-11-27 17:04:29 -0800

    created controller & views for Clients

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,7 +1,5 @@
 class ClientsController < ApplicationController
     def show
-        @clients = clients_helper.find_by_id(params[:id])
-        # #@children = child_helper.find_by_facility(params[:id])
-        # @complaints = complaint_helper.find_by_facility(params[:id])
+  
     end 
 end
