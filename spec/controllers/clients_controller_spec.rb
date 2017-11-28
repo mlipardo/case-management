a5529cae572832c4f87f9e97ffe6a88aab37bf32
Ramu Kammagani <cambriadeveloper@Cambrias-MacBook-Pro.local>
2017-11-28 12:07:27 -0800

    added Rspec tests on Client ID Page

--- spec/controllers/clients_controller_spec.rb
+++ spec/controllers/clients_controller_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 include RSpec
 describe ClientsController do
-describe 'GET show' do
+  describe '#show' do
+    it 'has route' do
+      expect(get: 'clients/show').to route_to(controller: 'clients', action: 'show')
+    end
+
     it 'renders show template' do
       get :show
+      expect(response).to render_template 'show'
     end
-end
+  end 
+
 end
